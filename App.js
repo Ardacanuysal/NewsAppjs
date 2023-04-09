db7984d187cf525a3edfd2b64f922d895f367b87
Arda Can Uysal <ardacanuysal03@gmail.com>
2023-04-10 01:59:07 +0300

    Update App.js

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default function App() {
               {news_banner_data.map(bannerNews => (
                 <Image
                   style={styles.banner_image}
+                
                   source={{uri: bannerNews.imageUrl}}
                 />
               ))}
